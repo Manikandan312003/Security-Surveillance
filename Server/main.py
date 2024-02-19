fc34be256aec5430968a5c27d613577b7f7c5c85
Mani <manirajendran31jul@gmail.com>
2024-02-19 18:41:05 +0530

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -6,8 +6,12 @@ from flask import request, jsonify, send_file
 from flask_cors import CORS
 import base64
 import datetime
+
 app = Flask(__name__)
 CORS(app)
+
+from flask_socketio import SocketIO
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 # SQL Operations
 import sqlite3
@@ -34,9 +38,12 @@ def makeConnection():
     return sqlite3.connect("securitysurveillance.db")
 
 
-def execute_query(query, connection=makeConnection(), datatuples=()):
-    cursor = connection.cursor()
+def execute_query(query, connection=None, datatuples=()):
+    cursor = None
     try:
+        if connection==None :
+            connection = makeConnection()
+            cursor = connection.cursor()
         cursor.execute(query, datatuples)
         connection.commit()
         connection.close()
@@ -57,6 +64,19 @@ def read_query(query="""Select *from officers;
         return result
     except Exception as readError:
         print(bcolors.redUnderline + f"Error:'{readError}''")
+
+def readWithColoumn(query='Select *from suspects;'):
+    try:
+        connection = makeConnection()
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        rows = [dict(row) for row in rows]
+        connection.close()
+        return rows
+    except Exception as readException:
+        return str(readException)
 
 
 def displayQuery(query="""Select *from officers;
@@ -91,14 +111,13 @@ reason varchar(255) Not Null,
 userid INTEGER Not Null);
 """
 
-
 # execute_query(createTableofSuspect,connection)
 
 
 # For client
 
 
-#Login and Password
+# Login and Password
 import bcrypt
 
 
@@ -107,8 +126,10 @@ def hashPassword(password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def verifyPassword(stored_password, provided_password):
     return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password)
+
 
 @app.route('/check', methods=['GET'])
 def checkPassword():
@@ -126,8 +147,8 @@ def checkPassword():
         else:
             return jsonify({"status": "User Not Exist"})
 
-        originalPassword = bytes(originalPassword[2:-1],'utf-8')
-        if verifyPassword(originalPassword,password):
+        originalPassword = bytes(originalPassword[2:-1], 'utf-8')
+        if verifyPassword(originalPassword, password):
             return jsonify({"status": "success", "username": userName, "useremail": useremail, "userid": userid})
         else:
             return jsonify({"status": "Wrong Password"})
@@ -171,7 +192,7 @@ def deleteSuspect():
         if suspectId == 0: return jsonify({"status": "required suspect id"})
         userId = request.args.get('userid', 0)
         if userId == 0: return jsonify({"status": "required user id"})
-
+        deleteSuspectDetails(suspectId)
         deleteQuery = f'delete from suspects where id={suspectId} and userid={userId}'
         (execute_query(deleteQuery, connection=makeConnection()))
         return jsonify({"status": "success"})
@@ -188,15 +209,17 @@ def editSuspect():
         if userId == 0: return jsonify({"status": "required user id"})
         name = request.args.get("name", "Not Mentioned")
         reason = request.args.get("reason", "Not Mentioned")
-        location = request.args.get("location","")
-        latitude = request.args.get("latitude","")
-        longitude = request.args.get("longitude","")
+        location = request.args.get("location", "")
+        latitude = request.args.get("latitude", "")
+        longitude = request.args.get("longitude", "")
 
         query = f'select location,latitude,longitude from suspects where id={suspectId}'
-        details = read_query(query,makeConnection())
+        details = read_query(query, makeConnection())
         if details:
-            if (location,latitude,longitude)!=details[0]:
-                addSuspectDetails(suspectId,*details[0])
+            newData = (location, latitude, longitude)
+            if newData != details[0]:
+                print(details[0], location, latitude, longitude)
+                addSuspectDetails(suspectId, *newData)
 
         editQuery = f'Update suspects set name="{name}",reason="{reason}",location ="{location}",latitude="{latitude}",longitude="{longitude}" where id="{suspectId}" and userid="{userId}";'
         execute_query(editQuery, connection=makeConnection())
@@ -223,7 +246,7 @@ def convertTobase64(query):
     return suspects
 
 
-@app.route('/getallsuspect')
+@app.route('/getallsuspect',methods = ['Get'])
 def getAllsuspect():
     try:
         getAllsuspectQuery = "Select * from suspects"
@@ -247,11 +270,12 @@ def getMySuspect():
         return jsonify({"status": "fail", "err": str(mysuspectError)})
 
 
-#GET Suspect Details
+# GET Suspect Details
 def load_data():
     with open('securitysurveillance.json', 'r') as file:
         data = json.load(file)
     return data
+
 
 def save_data(data):
     with open('securitysurveillance.json', 'w') as file:
@@ -266,46 +290,100 @@ def checkId(id):
     return False
 
 
-def addSuspectDetails(id,location,latitude,longitude):
+def addSuspectDetails(id, location, latitude, longitude):
     try:
-        locDet = [str(datetime.date.today()),location,latitude,longitude]
+        locDet = [str(datetime.date.today()), location, latitude, longitude]
         suspectDetails = load_data()
         if id in suspectDetails:
-            suspectDetails[id].insert(0,locDet)
+            suspectDetails[id].insert(1, locDet)
+            save_data(suspectDetails)
             return True
         else:
-
             userDet = convertTobase64(f'select name,image,reason from suspects where id={id}')[0]
-            suspectDetails[id] = [[userDet[0],userDet[2],userDet[1]],
+            suspectDetails[id] = [[userDet[0], userDet[2], userDet[1]],
                                   locDet]
             save_data(suspectDetails)
             return True
     except Exception as addException:
-        print('Err',addException)
+        print('Err', addException)
         return False
 
 
+def deleteSuspectDetails(id):
+    suspectDetails = load_data()
+    if id in suspectDetails:
+        del suspectDetails[id]
+        save_data(suspectDetails)
+        return True
+    return False
 
-@app.route('/getsuspectdetails',methods=['GET'])
+
+@app.route('/getsuspectdetails', methods=['GET'])
 def getsuspectDetails():
-    suspectId = request.args.get('suspectid',0)
-    if suspectId==0:
-        return jsonify({'status:invalid','suspectdetails'})
+    suspectId = request.args.get('suspectid', 0)
+    if suspectId == 0:
+        return jsonify({'status:invalid', 'suspectdetails'})
     try:
-        suspectDetail = load_data().get(suspectId,0)
+        suspectDetail = load_data().get(suspectId, 0)
         if suspectDetail:
-            print(suspectDetail)
             return jsonify({'status': 'success', 'suspectdetails': suspectDetail})
         return jsonify({
-            'status':'no history',
-            'suspectdetails':''
+            'status': 'no history',
+            'suspectdetails': ''
         })
     except:
         pass
 
-@app.route('/getdatabase',methods=['GET'])
+
+@app.route('/getdatabase', methods=['GET'])
 def getDataBase():
     pass
 
+
+
+#GSM
+
+nodeMcuConnects = set()
+
+
+
+@socketio.on('connect', namespace='/nodemcu')
+def onNodeMcuConnect():
+    print('nodeMCU Connected')
+    nodeMcuConnects.add(request.sid)
+    infos = readWithColoumn('select * from informtogsm')
+    if infos:
+        for info in infos:
+            socketio.emit('nodemcuget', info, namespace='/nodemcu')
+    execute_query('delete from informtogsm')
+
+
+@socketio.on('disconnect', namespace='/nodemcu')
+def onNodeMcuDisConnect():
+    nodeMcuConnects.remove(request.sid)
+    print('Node Disconnected')
+
+
+@socketio.on('sendtonodemcu')
+def nodemcu(data):
+    print(data)
+    if nodeMcuConnects != set():
+        socketio.emit('nodemcuget', data,namespace='/nodemcu')
+        print('nodemcu')
+    else:
+        query = f'''INSERT INTO informtogsm ( suspectName, contactNumber, location, category)
+VALUES (?, ?, ?, ?);'''
+        execute_query(query=query,datatuples=(data.get('name'), data.get('number'), data.get('location'), data.get('category')))
+        print(query, 'no nodemcu found')
+
+@app.route('/infogsm',methods=['POST'])
+def infoGsm():
+    nodemcu(request.form)
+
+@app.route('/',methods=['GET'])
+def index():
+    return jsonify({'Hi','Hi'})
+
 if __name__ == '__main__':
     app.run(debug=True)
+    # socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
